ff5b4b2bcce6ab6b128919414e072f5a849f22e2
adamheidrick <adamheidrick@gmail.com>
2022-10-10 16:36:32 +0200

    Fleshing out menu system

--- menus.py
+++ menus.py
@@ -1,14 +1,24 @@
-from PyInquirer import Separator
+from PyInquirer import Separator, style_from_dict, Token
 
 welcome = ' Welcome to Cycle Dad! \n\n This program will give you a clothing recommendation \n' \
                        ' based on your weather so you can plan that epic ride. \n' \
                        ' You can input your own zip code or it can be automatically generated. \n' \
                        ' Or . . . you can just have a dad joke if you want. Cheers. \n'
 
+style = style_from_dict({
+    Token.Separator: '#cc5454',
+    Token.QuestionMark: '#673ab7 bold',
+    Token.Selected: '#cc5454',  # default
+    Token.Pointer: '#673ab7 bold',
+    Token.Instruction: '',  # default
+    Token.Answer: '#f44336 bold',
+    Token.Question: '',
+})
+
 home_menu = [
             {
                 'type': 'list',
-                'qmark': '#',
+                'qmark': ' #',
                 'message': ' CycleDad: ',
                 'name': 'user_option',
                 'choices': [
@@ -33,18 +43,18 @@ home_menu = [
 zip_menu = [
             {
                 'type': 'input',
-                'qmark': '#',
+                'qmark': ' #',
                 'name': 'zip',
                 'message': 'What is your zip (5 digits): '
             },
 
             {
                 'type': 'list',
-                'qmark': '#',
+                'qmark': ' #',
                 'message': ' CycleDad: ',
                 'name': 'user_option',
                 'choices': [
-                    Separator('\n ======== Confirm, Redo, GoBack, DadJoke, Quit =========='),
+                    Separator('\n ======== Continue, Redo, GoBack, DadJoke, Quit =========='),
                     {
                         'name': 'Continue.'
                     },
@@ -58,6 +68,47 @@ zip_menu = [
                     {
                         'name': 'None of that, just give me a dad joke.'
                     },
+                    {
+                        'name': 'Quit Program'
+                    }
+                ]
+            }
+        ]
+
+dad_menu = [
+            {
+                'type': 'list',
+                'qmark': ' #',
+                'message': ' Easy Nav: ',
+                'name': 'user_option',
+                'choices': [
+                    Separator('\n ======== Go Home, Quit =========='),
+                    {
+                        'name': 'Go Home.'
+                    },
+
+                    {
+                        'name': 'Quit Program.'
+                    }
+                ]
+            }
+        ]
+
+auto_menu = [
+            {
+                'type': 'list',
+                'qmark': ' #',
+                'message': ' Easy Nav: ',
+                'name': 'user_option',
+                'choices': [
+                    Separator('\n ======== Is the Zip Code Correct? =========='),
+                    {
+                        'name': 'Yes, Confirm'
+                    },
+                    {
+                        'name': 'No, Go Back'
+                    },
+
                     {
                         'name': 'Quit Program'
                     }
--- ui.py
+++ ui.py
@@ -2,10 +2,8 @@ from __future__ import print_function, unicode_literals
 
 import os
 import menus
-from termcolor import colored
-from pprint import pprint
-from prompt_toolkit.validation import Validator, ValidationError
 
+from termcolor import colored
 from pyfiglet import Figlet
 from PyInquirer import prompt
 
@@ -15,19 +13,21 @@ class CycleDad:
         self.logoText = Figlet(font='speed')
         self.welcome = menus.welcome
         self.home_menu = menus.home_menu
+        self.easy_menu = menus.dad_menu
         self.zip_menu = menus.zip_menu
+        self.auto_menu = menus.auto_menu
         self.init = self.main()
 
     def main(self):
         os.system('clear')
         print("\n")
-        print(colored(self.logoText.renderText('  CYCLE DAD'), 'yellow'))
+        print(colored(self.logoText.renderText('CYCLE DAD'), 'cyan'))
         print(self.welcome)
         self.run()
         return 0
 
     def run(self):
-        answers = prompt(self.home_menu)
+        answers = prompt(self.home_menu, style=menus.style)
         if 'Quit Program' in answers['user_option']:
             os.system('clear')
             os.system('exit')
@@ -44,15 +44,15 @@ class CycleDad:
             self.enter_zip()
 
         elif answer == 'I want you to automatically find my zip.':
-            pass
+            self.auto_zip()
 
         else:
             pass
 
     def enter_zip(self):
         os.system('clear')
-        print(self.logoText.renderText('  ENTER ZIP'))
-        answer = prompt(self.zip_menu)
+        print(colored(self.logoText.renderText('Enter Zip'), 'cyan'))
+        answer = prompt(self.zip_menu, style=menus.style)
         if 'Go Back' in answer['user_option']:
             os.system('clear')
             self.main()
@@ -60,21 +60,26 @@ class CycleDad:
         if 'Redo' in answer['user_option']:
             self.enter_zip()
 
+        if "Continue." in answer['user_option']:
+            self.results()
+
     def auto_zip(self):
         os.system('clear')
         print("\n")
-        print(self.logoText.renderText('  Auto Zip Generator'))
-        print(" AUTO ZIP MICROSERVICE?")
+        print(colored(self.logoText.renderText('Auto Zip'), 'cyan'))
+        print(" AUTO ZIP MICROSERVICE. YOUR ZIP IS:")
+        print(colored(' ZIPCODE', 'green'))
         # TODO: use import socket to get gather IP Address
         # TODO: Find service to convert IP to Zip.
+        answers = prompt(self.auto_menu, style=menus.style)
         pass
 
     def dad_joke(self, zip_code=None):
         os.system('clear')
         print("\n")
-        print(self.logoText.renderText('  Dad Joke'))
-        print(" DAD JOKE GENERATOR MICROSERVICE")
-        # TODO: Find DAD JOKE API
+        print(colored(self.logoText.renderText('Dad Joke'), 'cyan'))
+        print(" DAD JOKE GENERATOR MICROSERVICE\n DAD JOKE WILL GO HERE\n")
+        answers = prompt(self.easy_menu, style=menus.style)
 
         if zip_code is None:
             # Find zip based off of IP
@@ -83,7 +88,13 @@ class CycleDad:
             # API weather app MICROSERVICE
             pass
 
-        # MENU
+    def results(self):
+        os.system('clear')
+        print("\n")
+        print(colored(self.logoText.renderText('Results'), 'cyan'))
+        answers = prompt(self.easy_menu, style=menus.style)
+
+        pass
 
 
 if __name__ == "__main__":

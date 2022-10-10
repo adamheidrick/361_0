63305c9d6d8207a93cfc42c97e79534513ba8525
adamheidrick <adamheidrick@gmail.com>
2022-10-10 11:41:07 +0200

    Making a class instead of function based.

--- ui.py
+++ ui.py
@@ -1,26 +1,109 @@
+from __future__ import print_function, unicode_literals
+
 import os
+from pprint import pprint
+from prompt_toolkit.validation import Validator, ValidationError
+
 from pyfiglet import Figlet
-from PyInquirer import prompt
-
-logoText = Figlet(font='speed')
-welcome = 'Welcome to Cycle Dad!'
-
-questions = [
-    {
-        'type': 'list',
-        'name': 'user_option',
-        'message': 'Welcome to Cycle Dad!',
-        'choices': ["Get My Zip", "Enter My Zip"]
-    }
-
-]
+from PyInquirer import prompt, Separator
 
 
-def main():
-    os.system('clear')
-    print(logoText.renderText('CYCLE DAD'))
-    os.system('clear')
+class CycleDad:
+    def __init__(self):
+        self.logoText = Figlet(font='speed')
+        self.welcome = 'Welcome to Cycle Dad!'
+        self.home_menu = [
+            {
+                'type': 'list',
+                'qmark': '#',
+                'message': ' CycleDad: ',
+                'name': 'user_option',
+                'choices': [
+                    Separator('\n ==  How would you like to enter your zip?  =='),
+                    {
+                        'name': 'I want you to automatically find my zip.'
+                    },
+                    {
+                        'name': 'Woah! I want to enter my own.'
+                    },
+                    {
+                        'name': 'None of that, just give me a dad joke.'
+                    },
+                    {
+                        'name': 'Quit Program'
+                    }
+                ]
+            }
+
+        ]
+        self.zip_menu = [
+            {
+                'type': 'input',
+                'name': 'zip',
+                'message': 'What is your zip (5 digits): '
+            },
+
+            {
+                'type': 'list',
+                'qmark': '#',
+                'message': ' CycleDad: ',
+                'name': 'user_option',
+                'choices': [
+                    Separator('\n ==  Is this working?  =='),
+                    {
+                        'name': 'Continue.'
+                    },
+                    {
+                        'name': 'Go Back.'
+                    },
+                    {
+                        'name': 'None of that, just give me a dad joke.'
+                    },
+                    {
+                        'name': 'Quit Program'
+                    }
+                ]
+            }
+
+
+
+
+
+        ]
+
+
+        self.init = self.main()
+
+    def main(self):
+        os.system('clear')
+        print("\n")
+        print(self.logoText.renderText('  CYCLE DAD'))
+        self.run()
+
+    def run(self):
+        answers = prompt(self.home_menu)
+        if 'Quit Program' in answers['user_option']:
+            os.system('exit')
+        else:
+            self.parse_answers(answers)
+
+    def parse_answers(self, answers):
+        answer = answers['user_option']
+        if answer == 'Woah! I want to enter my own.':
+            self.enter_zip()
+
+        elif answer == 'I want you to automatically find my zip.':
+            pass
+
+        else:
+            pass
+
+    def enter_zip(self):
+        print("")
+        answer = prompt(self.zip_menu)
+        pass
+
 
 
 if __name__ == "__main__":
-    main()
+    main = CycleDad()

c0d42d09b7b4ad063c868cc1a2c73cb78de5e200
adamheidrick <73691525+adamheidrick@users.noreply.github.com>
2022-10-26 15:38:26 +0200

    Update weatherAPI.py

--- weatherAPI.py
+++ weatherAPI.py
@@ -5,7 +5,7 @@ url = "https://weatherapi-com.p.rapidapi.com/current.json"
 querystring = {"q": "85603"}
 
 headers = {
-    "X-RapidAPI-Key": "27c0ac8818msh2ca32d6adfc0fb1p1d0a96jsn4c228f1a6b2a",
+    "X-RapidAPI-Key": "",
     "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
 }
 
